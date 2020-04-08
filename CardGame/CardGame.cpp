383b464c4a875570e2b5b6b60ccb17fae3e58079
Vincento1337 <55494266+Vincento1337@users.noreply.github.com>
2020-04-08 19:08:56 +0300

    Add files via upload

---
+++ CardGame/CardGame.cpp
@@ -0,0 +1,201 @@
+﻿#include <iostream>
+#include <array>
+#include <ctime> // для time()
+#include <cstdlib> // для rand() и srand()
+
+enum CardSuit
+{
+	SUIT_TREFU,
+	SUIT_BYBNU,
+	SUIT_CHERVU,
+	SUIT_PIKI,
+	MAX_SUITS
+};
+
+enum CardRank
+{
+	RANK_2,
+	RANK_3,
+	RANK_4,
+	RANK_5,
+	RANK_6,
+	RANK_7,
+	RANK_8,
+	RANK_9,
+	RANK_10,
+	RANK_VALET,
+	RANK_DAMA,
+	RANK_KOROL,
+	RANK_TYZ,
+	MAX_RANKS
+};
+
+struct Card
+{
+	CardRank rank;
+	CardSuit suit;
+};
+
+void printCard(const Card& card)
+{
+	switch (card.rank)
+	{
+	case RANK_2:		std::cout << "2"; break;
+	case RANK_3:		std::cout << "3"; break;
+	case RANK_4:		std::cout << "4"; break;
+	case RANK_5:		std::cout << "5"; break;
+	case RANK_6:		std::cout << "6"; break;
+	case RANK_7:		std::cout << "7"; break;
+	case RANK_8:		std::cout << "8"; break;
+	case RANK_9:		std::cout << "9"; break;
+	case RANK_10:		std::cout << "T"; break;
+	case RANK_VALET:	std::cout << "V"; break;
+	case RANK_DAMA:	        std::cout << "D"; break;
+	case RANK_KOROL:	std::cout << "K"; break;
+	case RANK_TYZ:		std::cout << "T"; break;
+	}
+
+	switch (card.suit)
+	{
+	case SUIT_TREFU:	std::cout << "TR"; break;
+	case SUIT_BYBNU:	std::cout << "B"; break;
+	case SUIT_CHERVU:	std::cout << "CH"; break;
+	case SUIT_PIKI:	        std::cout << "P"; break;
+	}
+}
+
+void printDeck(const std::array<Card, 52>& deck)
+{
+	for (const auto& card : deck)
+	{
+		printCard(card);
+		std::cout << ' ';
+	}
+
+	std::cout << '\n';
+}
+
+void swapCard(Card& a, Card& b)
+{
+	Card temp = a;
+	a = b;
+	b = temp;
+}
+
+int getRandomNumber(int min, int max)
+{
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+void shuffleDeck(std::array<Card, 52>& deck)
+{
+	for (int index = 0; index < 52; ++index)
+	{
+		int swapIndex = getRandomNumber(0, 51);
+		swapCard(deck[index], deck[swapIndex]);
+	}
+}
+
+int getCardValue(const Card& card)
+{
+	switch (card.rank)
+	{
+	case RANK_2:		return 2;
+	case RANK_3:		return 3;
+	case RANK_4:		return 4;
+	case RANK_5:		return 5;
+	case RANK_6:		return 6;
+	case RANK_7:		return 7;
+	case RANK_8:		return 8;
+	case RANK_9:		return 9;
+	case RANK_10:		return 10;
+	case RANK_VALET:	return 10;
+	case RANK_DAMA:	        return 10;
+	case RANK_KOROL:	return 10;
+	case RANK_TYZ:		return 11;
+	}
+
+	return 0;
+}
+
+char getPlayerChoice()
+{
+	std::cout << "(h) to hit, or (s) to stand: ";
+	char choice;
+	do
+	{
+		std::cin >> choice;
+	} while (choice != 'h' && choice != 's');
+
+	return choice;
+}
+
+bool playBlackjack(const std::array<Card, 52>& deck)
+{
+	const Card* cardPtr = &deck[0];
+
+	int playerTotal = 0;
+	int dealerTotal = 0;
+
+	dealerTotal += getCardValue(*cardPtr++);
+	std::cout << "The dealer is showing: " << dealerTotal << '\n';
+
+	playerTotal += getCardValue(*cardPtr++);
+	playerTotal += getCardValue(*cardPtr++);
+
+
+	while (1)
+	{
+		std::cout << "You have: " << playerTotal << '\n';
+
+
+		if (playerTotal > 21)
+			return false;
+
+		char choice = getPlayerChoice();
+		if (choice == 's')
+			break;
+
+		playerTotal += getCardValue(*cardPtr++);
+	}
+
+
+	while (dealerTotal < 17)
+	{
+		dealerTotal += getCardValue(*cardPtr++);
+		std::cout << "The dealer now has: " << dealerTotal << '\n';
+	}
+
+	if (dealerTotal > 21)
+		return true;
+
+	return (playerTotal > dealerTotal);
+}
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0)));
+	rand();
+
+	std::array<Card, 52> deck;
+
+	
+	int card = 0;
+	for (int suit = 0; suit < MAX_SUITS; ++suit)
+		for (int rank = 0; rank < MAX_RANKS; ++rank)
+		{
+			deck[card].suit = static_cast<CardSuit>(suit);
+			deck[card].rank = static_cast<CardRank>(rank);
+			++card;
+		}
+
+	shuffleDeck(deck);
+
+	if (playBlackjack(deck))
+		std::cout << "You win!\n";
+	else
+		std::cout << "You lose!\n";
+
+	return 0;
+}
